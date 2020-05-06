0848e9752a3aa0b42f3a90d81df1b639bc987bf5
Asif Mohammed <asifm2882@gmail.com>
2020-05-06 20:55:49 +0530

    add HTMLWebpackPlugin :zap:

--- public/iProtectU/react-planner-wrapper/webpack.config.js
+++ public/iProtectU/react-planner-wrapper/webpack.config.js
@@ -1,44 +1,69 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(jpe?g|png|gif|mtl|obj)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              hash: 'sha512',
-              digest: 'hex',
-              name: '[path][name].[ext]',
-              context: 'react-planner-wrapper/src',
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      'react-dom': '@hot-loader/react-dom',
+module.exports = (env, self) => {
+  let isProduction = self.hasOwnProperty('mode')
+    ? self.mode === 'production'
+    : true;
+  let publicPath = self.hasOwnProperty('publicPath') ? self.publicPath : '/';
+  let config = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath,
     },
-  },
-  devServer: {
-    contentBase: './dist',
-  },
-};
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(jpe?g|png|gif|mtl|obj)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                hash: 'sha512',
+                digest: 'hex',
+                name: '[path][name].[ext]',
+                context: path.resolve(__dirname, 'src'),
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [{ loader: 'style-loader/url' }, { loader: 'file-loader' }],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        'react-dom': '@hot-loader/react-dom',
+      },
+    },
+    devServer: {
+      contentBase: path.join(__dirname, './dist'),
+    },
+    plugins: [],
+  };
 
-module.exports = config;
+  if (!isProduction) {
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        title: 'react-planner',
+        template: './src/index.html.ejs',
+        filename: 'index.html',
+        inject: 'body',
+        production: isProduction,
+      })
+    );
+  }
+
+  return config;
+};
