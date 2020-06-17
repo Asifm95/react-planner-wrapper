23d21dd887a40319a7deb7889c5ae96ec6e70d4d
Asif Mohammed <asifm2882@gmail.com>
2020-06-17 16:16:21 +0530

    add submit button component

---
+++ public/iProtectU/react-planner-wrapper/src/style/submit-button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from './button'
+
+const STYLE = {
+  borderColor: "#415375",
+  backgroundColor: "#415375",
+  color: '#FFF'
+};
+
+const STYLE_HOVER = {
+  borderColor: "#1f3149",
+  backgroundColor: "#1f3149",
+  color: '#FFF'
+};
+
+export default function FormSubmitButton({children, ...rest}) {
+  return <Button type="submit" style={STYLE} styleHover={STYLE_HOVER} {...rest}>{children}</Button>
+}
